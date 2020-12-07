2afb062b5e21df7cb071963b53d4d8e75286cd79
Bryse <brysonmacduff@gmail.com>
2020-12-07 16:06:14 -0500

    second try

--- SuperTop.vhd
+++ SuperTop.vhd
@@ -10,6 +10,11 @@ end entity;
 
 architecture structure of SuperTop is
 
+component Clock_Divider is
+port (clk,reset: in bit;
+clock_out: out bit);
+end component;
+
 component TL_Top is -- traffic lights
 port(gReset,SSCS,i0_MSC,i1_MSC,i2_MSC,i3_MSC,i0_SSC,i1_SSC,i2_SSC,i3_SSC,clk: in bit; MS0_Green,MS1_Yellow,MS2_Red,SS0_Green,SS1_Yellow,SS2_Red: out bit);
 end component;
@@ -67,8 +72,11 @@ signal TSR_out_vector: bit_vector(8 downto 0) := "111111111";
 signal TDR_lsb: bit;
 signal TXD_temp: bit;
 signal spark: bit := '1';
+signal actual_logic_clock: bit;
 
 begin
+
+	CLK_DIV: Clock_Divider port map(logic_clock,'0',actual_logic_clock);
 
 	TOP: TL_Top port map(SW(9),SW(8),SW(4),SW(5),SW(6),SW(7),SW(0),SW(1),SW(2),SW(3),KEY0, -- inputs
 	ms_lights(0),ms_lights(1),ms_lights(2),ss_lights(0),ss_lights(1),ss_lights(2)); -- outputs
@@ -80,30 +88,30 @@ begin
 	LEDR(1) <= ss_lights(1);
 	LEDR(0) <= ss_lights(0);
 
-	MSTL0: tSr port map('0',irq,'0',logic_clock,mstl_0,mstl_0_1bit_out,mstl_0_out);
-	MSTL1: tSr port map('0',irq,'0',logic_clock,mstl_1,mstl_1_1bit_out,mstl_1_out);
-	MSTL2: tSr port map('0',irq,'0',logic_clock,mstl_2,mstl_2_1bit_out,mstl_2_out);
-	MSTL3: tSr port map('0',irq,'0',logic_clock,mstl_3,mstl_3_1bit_out,mstl_3_out);
-	MSTL4: tSr port map('0',irq,'0',logic_clock,mstl_4,mstl_4_1bit_out,mstl_4_out);
-	MSTL5: tSr port map('0',irq,'0',logic_clock,mstl_5,mstl_5_1bit_out,mstl_5_out);
+	MSTL0: tSr port map('0',irq,'0',actual_logic_clock,mstl_0,mstl_0_1bit_out,mstl_0_out);
+	MSTL1: tSr port map('0',irq,'0',actual_logic_clock,mstl_1,mstl_1_1bit_out,mstl_1_out);
+	MSTL2: tSr port map('0',irq,'0',actual_logic_clock,mstl_2,mstl_2_1bit_out,mstl_2_out);
+	MSTL3: tSr port map('0',irq,'0',actual_logic_clock,mstl_3,mstl_3_1bit_out,mstl_3_out);
+	MSTL4: tSr port map('0',irq,'0',actual_logic_clock,mstl_4,mstl_4_1bit_out,mstl_4_out);
+	MSTL5: tSr port map('0',irq,'0',actual_logic_clock,mstl_5,mstl_5_1bit_out,mstl_5_out);
 
 	Mux_3to8_9bit: Mux_8to1_9bit port map (mstl_0_out,mstl_1_out,mstl_2_out,mstl_3_out,mstl_4_out,mstl_5_out,ground,ground,S0,S1,S2,mux_out_vector);
 
-	TDR_Register: tSr port map('0',load_TDR,'0',logic_clock,mux_out_vector, -- in bits
+	TDR_Register: tSr port map('0',load_TDR,'0',actual_logic_clock,mux_out_vector, -- in bits
 			TDR_lsb,TDR_out_vector); -- out bits
 	
-	TSR_Register: tSr port map('0',load_TSR,shift_TSR,logic_clock,TDR_out_vector, -- in bits
+	TSR_Register: tSr port map('0',load_TSR,shift_TSR,actual_logic_clock,TDR_out_vector, -- in bits
 			TXD_temp,TSR_out_vector); -- out bits
 
 	TXD <= TXD_temp or spark;
 
-	Generator_IRQ: irq_generator port map(ms_lights,ss_lights,logic_clock, -- in bits
+	Generator_IRQ: irq_generator port map(ms_lights,ss_lights,actual_logic_clock, -- in bits
 			irq,mstl_0,mstl_1,mstl_2,mstl_3,mstl_4,mstl_5); -- out bits
 	
-	TransmitterLogic: TL port map(logic_clock,irq, -- in bits
+	TransmitterLogic: TL port map(actual_logic_clock,irq, -- in bits
 			load_TDR,load_TSR,shift_TSR,S2,S1,S0); -- out bits
 
-	process(logic_clock)
+	process(actual_logic_clock)
 	begin
 	if shift_TSR = '1' then
 		spark <= '0';
